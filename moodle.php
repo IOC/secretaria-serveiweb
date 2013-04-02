4dea8965114e63f019fd9bf54f2a19c0c29578a3
Albert Gasset <albert.gasset@gmail.com>
2013-04-02 12:19:56 +0200

    increment timeout connexions HTTP

--- moodle.php
+++ moodle.php
@@ -190,7 +190,7 @@ class Moodle {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3600);
         $response = curl_exec($ch);
         $error = curl_error($ch);
         curl_close($ch);
